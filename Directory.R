33662c736e8342e673e842592bcf1c55a09a50c2
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-27 08:44:04 -0400

    new: location of important directories in file system

---
+++ Directory.R
@@ -0,0 +1,19 @@
+# Directory.R
+# determine directory for a given set of files
+
+Directory <- function(description) {
+    # return path to the described directory, as a string
+    # ARGS
+    # directory: chr
+    data.dir = '../../los-angeles/'
+    switch(
+        description
+        ,source  = './'
+        ,output  = paste0(data.dir, 'output/')
+        ,raw     = paste0(data.dir, 'raw/')
+        ,working = paste0(data.dir, 'working/')
+        ,realestate = '../../lowrancerealestater'
+        ,utilities  = '../../lowranceutilitiesr'
+        ,stop(paste('bad description', description))
+    )
+}
