295e1315e5bb2fdd592277a550a7a7e19fb7a6d5
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-30 12:29:56 -0400

    read census.Rdata

---
+++ ReadCensus.R
@@ -0,0 +1,7 @@
+ReadCensus <- function(path) {
+    cat('start ReadCensus', path, '\n'); browser()
+    census <- NULL
+    loaded <- load(path)  # may load more than parcels.sfr
+    stopifnot(!is.null(census))
+    census
+}
