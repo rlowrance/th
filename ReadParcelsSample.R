8dcd76159228adf1cbcce6d4c8670cec11cd5560
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-31 10:46:14 -0400

    rename to read parcels sfr sample

--- ReadParcelsSample.R
+++
@@ -1,8 +0,0 @@
-ReadParcelsSample <- function(path) {
-    # Return data frame for parcels sample
-    #cat('start ReadParcelsSample', path, '\n'); browser()
-    parcels.sample <- NULL
-    loaded <- load(path)  # may load more than parcels.sfr
-    stopifnot(!is.null(parcels.sample))
-    parcels.sample
-}
