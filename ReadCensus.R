e2d3d6dc3d4021c2d7789d0852116d731b28ef95
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-31 10:41:41 -0400

    disable browser()

--- ReadCensus.R
+++ ReadCensus.R
@@ -1,5 +1,5 @@
 ReadCensus <- function(path) {
-    cat('start ReadCensus', path, '\n'); browser()
+    #cat('start ReadCensus', path, '\n'); browser()
     census <- NULL
     loaded <- load(path)  # may load more than parcels.sfr
     stopifnot(!is.null(census))
