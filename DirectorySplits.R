2e9ac92c87fce1fc50bb567f374eb7caa1b698dd
Roy Lowrance <roy.lowrance@gmail.com>
2014-09-13 10:02:56 -0400

    revise directory name

--- DirectorySplits.R
+++ DirectorySplits.R
@@ -1,4 +1,4 @@
 source('DirectoryWorking.R')
 DirectorySplits <- function() {
-    paste0(DirectoryWorking(), 'transactions-al-sfr-subset1-splits/')
+    paste0(DirectoryWorking(), 'transactions-subset1-splits/')
 }
