19b431f52b38ea58b8b9cda03ba0418209bfe0c9
Roy Lowrance <roy.lowrance@gmail.com>
2014-09-19 11:10:11 -0400

    fix documentation

--- ReadTransactions.R
+++ ReadTransactions.R
@@ -1,6 +1,5 @@
 ReadTransactions <- function(path) {
     # return data.frame with all the transaction in the path
-    #cat('start ReadParcelsSfr', path, '\n'); browser()
     transactions <- NULL
     loaded <- load(path)  # may load more than transactions
     str(loaded)
