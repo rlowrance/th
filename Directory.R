23882885063823e387691142279c5ed6dd50363d
Roy Lowrance <roy.lowrance@gmail.com>
2014-09-26 09:48:20 -0400

    new: paths to all directories

---
+++ Directory.R
@@ -0,0 +1,17 @@
+Directory <- function(name) {
+    # convert the name of a directory into a path to that directory
+    # a path is relative to the source directory
+    # a path is a character that ends in a slash
+    data <- '../../los-angeles/'
+    path <- switch( name
+                   ,log = paste0(data, 'log/')
+                   ,output = paste0(data, 'output/')
+                   ,raw = paste0(data, 'raw/')
+                   ,source = '../../thesis/src/'
+                   ,splits = paste0(data, 'working/transactions-subset1-train-splits/')
+                   ,utilities = '../../lowranceutilitiesr'
+                   ,working = paste0(data, 'working/')
+                   )
+    path
+}
+
