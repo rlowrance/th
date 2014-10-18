371d5961c1970f2d6c952d5992e74e922b63d7c4
Roy Lowrance <roy.lowrance@gmail.com>
2014-10-18 08:18:45 -0400

    remove default args

--- e-reduced-features-chart.R
+++ e-reduced-features-chart.R
@@ -348,7 +348,7 @@ Main <- function(control) {
 
 #debug(Control)
 default.args <- NULL  # synthesize the command line that will be used in the Makefile
-default.args <- list('--query', '10000')
+#default.args <- list('--query', '10000')
 
 command.args <- if (is.null(default.args)) commandArgs(trailingOnly = TRUE) else default.args
 control <- Control(command.args)
