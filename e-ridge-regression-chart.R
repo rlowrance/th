5ac21547707fcbe51e16b4caf9bcc94ac95ef392
Roy Lowrance <roy.lowrance@gmail.com>
2014-10-24 22:17:15 -0400

    add chart 7; delete dead code

--- e-ridge-regression-chart.R
+++ e-ridge-regression-chart.R
@@ -50,6 +50,7 @@ Control <- function(default.args) {
                     ,path.out.chart4 = paste0(working, out.base, '_4.pdf')
                     ,path.out.chart5 = paste0(working, out.base, '_5.pdf')
                     ,path.out.chart6 = paste0(working, out.base, '_6.pdf')
+                    ,path.out.chart7 = paste0(working, out.base, '_7.pdf')
                     ,chart.width = 14  # inches
                     ,chart.height = 10 # inches
                     ,testing = testing
@@ -72,228 +73,6 @@ ParseCommandArgs <- function(command.args) {
                       ,positional_arguments = FALSE
                       )
 }
-Heading.1.and.2OLD <- function(control) {
-    result <- c( sprintf( 'cv.result of %f Percent Random Sample of Training Transactions'
-                        ,control$query.sample * 100
-                        )
-                ,'AVM scenario'
-                ,'Log-linear form'
-                ,sprintf( 'Testing period: %s through %s'
-                         ,control$testing.period$first.date
-                         ,control$testing.period$last.date
-                         )
-                ,sprintf( 'Number of training days: %d'
-                         ,control$num.training.days
-                         )
-                )
-    result
-}
-Chart1OLD <- function(control, cv.result, ordered.features) {
-    # text file with variables in order
-    Body <- function() {
-        # return a vector lines, the body of chart 1
-        FeatureNames <- function() {
-            c( 'Feature names ordered by importance'
-              ,'1 == most important'
-              ,sapply( 1:length(ordered.features)
-                      ,function(index) 
-                          sprintf(' %2d: %s'
-                                  ,index
-                                  ,ordered.features[[index]]
-                                  )
-                      )
-              )
-        }
-        result <- FeatureNames()
-        result
-    }
-    result <- c( Heading.1.and.2(control)
-                ,' '
-                ,Body()
-                )
-    result
-}
-Chart2OLD <- function(control, cv.result, ordered.features) {
-    # text file with median of rootMedianSquaredErrors
-    Body <- function() {
-        RMedianSE <- function(model.index) {
-            model.result <- cv.result[[model.index]]
-            rMedianSE.values <- sapply( 1:length(model.result)
-                                       ,function(fold.index) {
-                                           evaluate <- model.result[[fold.index]]
-                                           evaluate$rootMedianSquaredError
-                                       }
-                                       )
-            result <- median(rMedianSE.values)
-            result
-        }
-        result <- sapply( 1:length(cv.result)
-                         ,function(model.index) {
-                             sprintf( ' RMedianSE for features 1 through %2d = %f'
-                                     ,model.index
-                                     ,RMedianSE(model.index)
-                                     )
-                         }
-                         )
-        result
-    }
-    result <- c( Heading.1.and.2(control)
-                ,' '
-                ,Body()
-                )
-    result
-}
-Chart3OLD <- function(control, cv.result, ordered.features) {
-    # box chart showing all rMedianSE values in each fold
-    df <- NULL
-    for (cv.result.index in 1:length(cv.result)) {
-        one.cv <- cv.result[[cv.result.index]]
-        for (fold.index in 1:length(one.cv)) {
-            next.df <- data.frame( num.vars = cv.result.index
-                                  ,rootMedianSquaredError = one.cv[[fold.index]]$rootMedianSquaredError
-                                  )
-            df <- rbind(df, next.df)
-        }
-    }
-    df$num.vars <- factor(df$num.vars)
-    gg <- ggplot( df
-                 ,aes( x = num.vars
-                      ,y = rootMedianSquaredError
-                      )
-                )
-    g <- gg + geom_boxplot()
-    g
-}
-Chart4OLD <- function(control, cv.result, ordered.features) {
-    # median of RMedianSE
-    # line graph showing rMedianSE values in each fold with error bars
-    # ref: R Graphics Cookbook p158 (adding error bars to line graph)
-    # ref: R Graphics Cookbook, recipe 15.18 (calculated standard errors)
-    # NOTE: se is the standard deviation / sqrt(sample size)
-    # NOTE: a 95% confidence interval for the mean is +- 1.96 * se
-    df <- NULL
-    for (cv.result.index in 1:length(cv.result)) {
-        one.cv <- cv.result[[cv.result.index]]
-        for (fold.index in 1:length(one.cv)) {
-            next.df <- data.frame( num.vars = cv.result.index
-                                  ,rootMedianSquaredError = one.cv[[fold.index]]$rootMedianSquaredError
-                                  )
-            df <- rbind(df, next.df)
-        }
-    }
-
-    # compute median results and standard errors
-    ResultsForModel <- function(model.index) {
-        values <- df[df$num.vars == model.index, 'rootMedianSquaredError']
-        values
-    }
-    MedianRMedianSE <- function(model.index) {
-        values <- ResultsForModel(model.index)
-        values.median <- median(values)
-        values.median
-    }
-    SE <- function(model.index) {
-        # standard error of the mean
-        values <- ResultsForModel(model.index)
-        values.sd <- sd(values)
-        values.se <- values.sd / length(values)
-        values.se
-    }
-
-    model.index <- 1:length(cv.result)
-    medianRMedianSE <- sapply(1:length(cv.result), MedianRMedianSE)
-    se <- sapply(1:length(cv.result), SE)
-
-    df2 <- data.frame( model.index = model.index
-                      ,medianRMedianSE = medianRMedianSE
-                      ,se = se
-                      )
-    
-
-    gg <- ggplot( df2
-                 ,aes( x = model.index
-                      ,y = medianRMedianSE
-                      )
-                 )
-    g <- 
-        gg + 
-        geom_line(aes(group = 1)) + 
-        geom_point(size = 4) + 
-        geom_errorbar( aes( ymin = medianRMedianSE - se
-                           ,ymax = medianRMedianSE + se
-                           )
-                      ,width = 0.2
-                      )
-        
-
-        
-    g
-}
-Chart5OLD <- function(control, cv.result, ordered.features) {
-    # mean of rootMedianSquaredError
-    # line graph showing rMedianSE values in each fold with error bars
-    # ref: R Graphics Cookbook p158 (adding error bars to line graph)
-    # ref: R Graphics Cookbook, recipe 15.18 (calculated standard errors)
-    # NOTE: se is the standard deviation / sqrt(sample size)
-    # NOTE: a 95% confidence interval for the mean is +- 1.96 * se
-    df <- NULL
-    for (cv.result.index in 1:length(cv.result)) {
-        one.cv <- cv.result[[cv.result.index]]
-        for (fold.index in 1:length(one.cv)) {
-            next.df <- data.frame( num.vars = cv.result.index
-                                  ,rootMedianSquaredError = one.cv[[fold.index]]$rootMedianSquaredError
-                                  )
-            df <- rbind(df, next.df)
-        }
-    }
-
-    # compute median results and standard errors
-    ResultsForModel <- function(model.index) {
-        values <- df[df$num.vars == model.index, 'rootMedianSquaredError']
-        values
-    }
-    MeanRMedianSE <- function(model.index) {
-        values <- ResultsForModel(model.index)
-        values.mean <- mean(values)
-        values.mean
-    }
-    SE <- function(model.index) {
-        # standard error of the mean
-        values <- ResultsForModel(model.index)
-        values.sd <- sd(values)
-        values.se <- values.sd / length(values)
-        values.se
-    }
-
-    model.index <- 1:length(cv.result)
-    meanRMedianSE <- sapply(1:length(cv.result), MeanRMedianSE)
-    se <- sapply(1:length(cv.result), SE)
-
-    df2 <- data.frame( model.index = model.index
-                      ,meanRMedianSE = meanRMedianSE
-                      ,se = se
-                      )
-    
-
-    gg <- ggplot( df2
-                 ,aes( x = model.index
-                      ,y = meanRMedianSE
-                      )
-                 )
-    g <- 
-        gg + 
-        geom_line(aes(group = 1)) + 
-        geom_point(size = 4) + 
-        geom_errorbar( aes( ymin = meanRMedianSE - se
-                           ,ymax = meanRMedianSE + se
-                           )
-                      ,width = 0.2
-                      )
-        
-
-        
-    g
-}
 Charts <- function(my.control) {
     # produce all the charts
 
@@ -306,7 +85,11 @@ Charts <- function(my.control) {
     stopifnot(!is.null(cv.result))
     stopifnot(!is.null(model.name))
 
-    charts <- CrossValidateCharts(control, cv.result, model.name)
+    charts <- CrossValidateCharts( control = control
+                                  ,cv.result = cv.result
+                                  ,model.name = model.name
+                                  ,chart7 = list(lambda = control$lambda)
+                                  )
 
     writeLines( text = charts$chart1
                ,con = my.control$path.out.chart1
@@ -343,6 +126,13 @@ Charts <- function(my.control) {
         )
     print(charts$chart6)
     dev.off()
+
+    pdf( file = my.control$path.out.chart7
+        ,width = my.control$chart.width
+        ,height = my.control$chart.height
+        )
+    print(charts$chart7)
+    dev.off()
 }
 Main <- function(control) {
     InitializeR(duplex.output.to = control$path.out.log)
@@ -366,7 +156,7 @@ default.args <-
              )
     } else {
         list( query = 100
-             ,lambdaSet = 'one'
+             ,lambdaSet = 'd'
              )
     }
 
