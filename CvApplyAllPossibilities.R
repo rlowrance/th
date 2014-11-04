ea2e29ffde1f6df195a350bdfa365d5768856313
Roy Lowrance <roy.lowrance@gmail.com>
2014-11-04 17:59:51 -0500

    extend API to allow FUN to have one argument

--- CvApplyAllPossibilities.R
+++ CvApplyAllPossibilities.R
@@ -1,9 +1,10 @@
-CvApplyAllPossibilities <- function(f, possible) {
-    # f(possible$scope, possible$model, ..., possible$mtry), for all combinations, returning nothing
+CvApplyAllPossibilities <- function(FUN, possible, one.arg = FALSE) {
+    # FUN(possible$scope, possible$model, ..., possible$mtry), for all combinations, returning nothing
     # f should accumulate its results
     # ARGS:
-    # f       :  function(scope,model,timePeriod,scenario,response,predictorsName,predictorsForm
-    #                    ,ndays,query,C,ntree,mtry)
+    # FUN     :  function(scope,model,timePeriod,scenario,response,predictorsName,predictorsForm
+    #                    ,ndays,query,C,ntree,mtry); OR
+    # FUN     :  function(arg) where arg is a named list
     # possible: named list with elements scope, model, ... mtry
     #           each element is a vector of possible values
     
@@ -19,19 +20,36 @@ CvApplyAllPossibilities <- function(f, possible) {
     for (c              in possible$c) {
     for (ntree          in possible$ntree) {
     for (mtry           in possible$mtry) {
-        f( scope = scope
-          ,model = model
-          ,timePeriod = timePeriod
-          ,scenario = scenario
-          ,response = response
-          ,predictorsName = predictorsName
-          ,predictorsForm = predictorsForm
-          ,ndays = ndays
-          ,query = query
-          ,c = c
-          ,ntree = ntree
-          ,mtry = mtry
-          )
+        if (one.arg) {
+            arg <- list( scope = scope
+                        ,model = model
+                        ,timePeriod = timePeriod
+                        ,scenario = scenario
+                        ,response = response
+                        ,predictorsName = predictorsName
+                        ,predictorsForm = predictorsForm
+                        ,ndays = ndays
+                        ,query = query
+                        ,c = c
+                        ,ntree = ntree
+                        ,mtry = mtry
+                        )
+            FUN(arg)
+        } else {
+            FUN( scope = scope
+                ,model = model
+                ,timePeriod = timePeriod
+                ,scenario = scenario
+                ,response = response
+                ,predictorsName = predictorsName
+                ,predictorsForm = predictorsForm
+                ,ndays = ndays
+                ,query = query
+                ,c = c
+                ,ntree = ntree
+                ,mtry = mtry
+                )
+        }
     }
     }
     }
