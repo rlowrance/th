4f0af0bedd982b5b7e379d1adb9812fd1b3815b7
Roy Lowrance <roy.lowrance@gmail.com>
2014-11-07 09:30:55 -0500

    new: accumlate lines

---
+++ Lines.R
@@ -0,0 +1,23 @@
+Lines <- function(max.size = 1000) {
+  # return function object for a list of lines
+  # methods:
+  # $Append(line) --> append to lines
+  # $Get()        --> return all lines
+
+    lines <- rep('', max.size)
+    last.index <- 0
+
+    Append <- function(line, debug = FALSE) {
+        if (debug) browser()
+        last.index <<- last.index + 1
+        lines[[last.index]] <<- line
+    }
+
+    Get <- function() {
+        lines[1:last.index]
+    }
+
+    list( Append = Append
+         ,Get    = Get
+         )
+}
