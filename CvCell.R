076cc8cb035382c434baa3d1d74ada82e35f91e5
Roy Lowrance <roy.lowrance@gmail.com>
2014-11-14 12:17:42 -0500

    add FixedCellValues()

--- CvCell.R
+++ CvCell.R
@@ -1,5 +1,10 @@
 CvCell <- function() {
   # list of functions for working with e-cv-cells
+  # $Command(scope, model, ..., mtry) --> chr, Rscript -e-cv.R --scope SCOPE ... --mtry MTRY
+  # $Path(scope, model, ..., mtry) --> chr, path to WORKING/e-cv-cells/FILE.Rdata
+  # $Possible.Ndays() --> chr vector, all possible values for ndays, namely '30' .. '360'
+  # $Possible.PredictorsNames() --> chr vector, all possible values for predictorsName, 'always', ...
+  # $FixedCellValues(chart.name) --> list of any 12 cell attributes that are common
   source('Directory.R')
 
   Is.Valid.Scope <- function(s)          is.character(s)
@@ -15,6 +20,87 @@ CvCell <- function() {
   Is.Valid.Ntree <- function(s)          is.character(s) && as.integer(s) >= 0
   Is.Valid.Mtry <- function(s)           is.character(s) && as.integer(s) >= 0
 
+  FixedCellValues <- function(chart.name) {
+    switch(chart.name
+           ,Chart5 = list( scope = 'global'
+                          ,model = 'linear'
+                          ,timePeriod = '2008'
+                          ,scenario = 'avm'
+                          ,query = '1'
+                          ,lambda = '0'
+                          ,ntree = '0'
+                          ,mtry = '0'
+                          )
+           ,Chart6 = list( scope = 'global'
+                          ,model = 'linear'
+                          ,timePeriod = '2003on'
+                          ,scenario = 'avm'
+                          ,query = '100'
+                          ,lambda = '0'
+                          ,ntree = '0'
+                          ,mtry = '0'
+                          )
+           ,Chart7 =
+           ,Chart8 = list( scope = 'global'
+                          ,model = 'linear'
+                          ,scenario = 'avm'
+                          ,timePeriod = '2003on'
+                          ,predictorsName = 'alwaysNoAssessment'
+                          ,query = '100'
+                          ,lambda = '0'
+                          ,ntree = '0'
+                          ,mtry = '0'
+                          )
+           ,Chart9 =
+           ,Chart10 =
+           ,Chart11 = list( scope = 'global'
+                          ,model = 'linear'
+                          ,timePeriod = '2003on'
+                          ,scenario = 'avm'
+                          ,response = 'logprice'
+                          ,predictorsForm = 'level'
+                          ,ndays = '60'
+                          ,query = '100'
+                          ,lambda = '0'
+                          ,ntree = '0'
+                          ,mtry = '0'
+                          )
+           ,Chart12 = list( scope = 'global'
+                           ,model = 'linL2'
+                           ,timePeriod = '2003on'
+                           ,scenario = 'avm'
+                           ,response = 'logprice'
+                           ,predictorsName = 'best20'
+                           ,predictorsForm = 'level'
+                           ,ndays = '60'
+                           ,query = '100'
+                           ,ntree = '0'
+                           ,mtry = '0'
+                           )
+           ,Chart13 = list( model = 'linL2'
+                           ,timePeriod = '2003on'
+                           ,scenario = 'avm'
+                           ,response = 'logprice'
+                           ,predictorsForm = 'level'
+                           ,query = '100'
+                           ,ndays = '60'
+                           ,lambda = '400'
+                           ,ntree = '0'
+                           ,mtry = '0'
+                           )
+           ,Chart14 = list( scope = 'global'
+                           ,model = 'rf'
+                           ,scenario = 'avm'
+                           ,timePeriod = '2003on'
+                           ,response = 'logprice'
+                           ,predictorsForm = 'level'
+                           ,ndays = '60'
+                           ,query = '100'
+                           ,lambda = '0'
+                           )
+           ,stop(paste0('not implemented', chart.name))
+           )
+  }
   
   Command <- function( scope, model, timePeriod, scenario
                       ,response, predictorsName, predictorsForm, ndays
@@ -106,7 +192,9 @@ CvCell <- function() {
   }
                           
 
-  list( Command                  = Command
+  
+  list( FixedCellValues          = FixedCellValues
+       ,Command                  = Command
        ,Path                     = Path
        ,Possible.Ndays           = Possible.Ndays
        ,Possible.PredictorsNames = Possible.PredictorsNames
