afa53321e02486475fcde042f10fda692b940886
Roy Lowrance <roy.lowrance@gmail.com>
2014-11-27 08:24:30 -0500

    remove dead code

--- Chart7.R
+++ Chart7.R
@@ -1,12 +1,3 @@
-#source('CvCell.R')
-#source('HeadersFixed.R')
-#source('MeanWithin10.R')
-#source('MeanRMSE.R')
-#source('MedianRMSE.R')
-#source('RootMeanSquaredErrors.R')
-#source('RootMedianSquaredErrors.R')
-#source('Table7AHorizontal.R')
-
 Chart7 <- function(my.control) {
     # return txt lines for chart 7
 
@@ -45,6 +36,7 @@ Chart7 <- function(my.control) {
     Header <- function(lines, include.asterisk, query) {
         # mutate lines by appending the header
         lines$Append('Comparison of Metrics From from 10 Fold Cross Validation')
+        lines$Append('Mean of Root Mean Squared Errors (meanRMSE) vs.')
         lines$Append('Median of Root Median Squared Errors (medRMSE) vs.')
         lines$Append('Mean of Fraction of Predictions Within 10 Percent of Actual Values (fctWI10)')
         if (include.asterisk)
@@ -66,140 +58,6 @@ Chart7 <- function(my.control) {
                                      )
                              )
         )
-    }
-    AppendTableHorizontal <- function(lines, query) {
-        parts.a.and.b <- FALSE
-
-        PartA <- function(lines) {
-            table <- Table7AHorizontal(lines)
-            if (parts.a.and.b)
-                table$Append('Part A: All Results')
-            table$Append(' ')
-            table$Header1('preds', 'ndays')
-            table$Header2('response', 'form', 'Metric',
-                          '30', '60', '90', '120', '150', '180', '210', '240', '270', '300', '330', '360'
-                          )
-
-            DetailLine <- function(response, predictorsForm, metricName) {
-                Value <- function(ndays) {
-                    cv.result <- ACvResult( ndays = ndays
-                                           ,response = response
-                                           ,predictorsForm = predictorsForm
-                                           ,query = query
-                                           )
-                    result <-
-                        if (length(cv.result) == 1 ) { 
-                            NA
-                        } else {
-                            switch( metricName
-                                   ,medRMSE = MedianRMSE(cv.result)
-                                   ,fctWI10 = MeanWithin10(cv.result)
-                                   ,meanRMSE = MeanRMSE(cv.result)
-                                   ,stop('bad metricName')
-                                   )
-                        }
-                    result
-                }
-
-                Detail <- switch( metricName
-                                 ,medRMSE = table$DetailWholeNumbers
-                                 ,fctWI10 = table$DetailFractions
-                                 ,meanRMSE = table$DetailWholeNumbers
-                                 )
-
-                Detail( response, predictorsForm, metricName
-                       ,Value(30),   Value(60),  Value(90), Value(120), Value(150), Value(180)
-                       ,Value(210), Value(240), Value(270), Value(300), Value(330), Value(360)
-                       )
-            }
-
-            for (response in c('price', 'logprice')) {
-                for (predictorsForm in c('level', 'log')) {
-                    table$Append(' ')
-                    for (metricName in c('meanRMSE', 'medRMSE', 'fctWI10')) {
-                        DetailLine( response = response
-                                   ,predictorsForm = predictorsForm
-                                   ,metricName = metricName
-                                   )
-                    }
-                }
-            }
-        }
-        PartB <- function(lines) {
-            table <- Table.7.B(lines)
-            table$Append('Part B: Best result')
-            table$Append(' ')
-            table$Header(       ' ', 'pred',      ' ', 'Best',   'Best')
-            table$Header('response', 'form', 'metric', 'ndays', 'value')
-
-            DetailLine <- function(response, predictorsForm, metricName, table) {
-                Value <- function(ndays) {
-                    cv.result <- ACvResult( ndays = ndays
-                                           ,response = response
-                                           ,predictorsForm = predictorsForm
-                                           ,query = query
-                                           )
-                    result <-
-                        if (length(cv.result) == 1) {
-                            NA
-                        } else {
-                            switch( metricName
-                                   ,medRMSE = MedianRMSE(cv.result)
-                                   ,fctWI10 = MeanWithin10(cv.result)
-                                   ,stop('bad metricName')
-                                   )
-                        }
-                    result
-                }
-                DetailLineMedRMSE <- function() {
-                    values <- c( Value('30'), Value('60'), Value('90')
-                                ,Value('120'), Value('150'), Value('180')
-                                ,Value('210'), Value('240'), Value('270')
-                                ,Value('300'), Value('330'), Value('360'))
-                    best.value <- min(values)
-                    best.index <- which.min(values)
-                    table$DetailWholeNumber( response, predictorsForm, metricName
-                                            ,30 * best.index, best.value
-                                            )
-                }
-                DetailLineFctWI10 <- function() {
-                    values <- c( Value('30'), Value('60'), Value('90')
-                                ,Value('120'), Value('150'), Value('180')
-                                ,Value('210'), Value('240'), Value('270')
-                                ,Value('300'), Value('330'), Value('360'))
-                    best.value <- max(values)
-                    best.index <- which.max(values)
-                    table$DetailFraction( response, predictorsForm, metricName
-                                         ,30 * best.index, best.value
-                                         )
-                }
-                switch( metricName
-                       ,medRMSE = DetailLineMedRMSE()
-                       ,fctWI10 = DetailLineFctWI10()
-                       ,stop('bad metricName')
-                       )
-            }
-
-            for (metricName in c('medRMSE', 'fctWI10')) {
-                for (response in c('price', 'logprice')) {
-                    for (predictorsForm in c('level', 'log')) {
-                        DetailLine( response = response
-                                   ,predictorsForm = predictorsForm
-                                   ,metricName = metricName
-                                   ,table = table
-                                   )
-                    }
-                }
-            }
-        }
-
-
-        lines$Append(' ')
-        PartA(lines)
-        if (parts.a.and.b) {
-            lines$Append(' ')
-            PartB(lines)
-        }
     }
     AppendTableVertical <- function(lines, query) {
         # append to Lines() object
@@ -274,13 +132,6 @@ Chart7 <- function(my.control) {
         AppendTable(lines, query)
         lines
     }
-    ReportHorizontal <- function() {
-        result <- Report( AppendTable = AppendTableHorizontal
-                         ,query = '100'  # 1% sample
-                         ,include.asterisk = TRUE
-                         )
-        result
-    }
     ReportVertical <- function(query) {
         result <- Report( AppendTable = AppendTableVertical
                          ,query = query
@@ -290,9 +141,7 @@ Chart7 <- function(my.control) {
     }
 
 
-    result <- list( horizontal   = ReportHorizontal()$Get()
-                   ,vertical.1   = ReportVertical('100')$Get()  # 1% sample
-                   ,vertical.100 = ReportVertical('1')$Get()    # 100% sample
+    result <- list(txt = ReportVertical('1')$Get()    # 100% sample
                    )
     result
 }
